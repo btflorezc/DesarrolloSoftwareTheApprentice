84af9947720eaf033983032d05f03f71dbbd240c
amalzate821 <ana.alzate@udea.edu.co>
2021-10-14 11:02:07 -0500

    React copiado desde rama

---
+++ misiontic-shop-antonela/src/gestion-usuarios/GestionUsuarios.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+function GestionUsuarios() {
+
+    return (
+        <Fragment>
+            <h1>Gestión de Usuarios</h1>
+        </Fragment>
+    )
+}
+
+export default GestionUsuarios;
---
+++ misiontic-shop-antonela/src/register/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import '../register/registerStyles.css';
+
+function RegisterPage() {
+
+    return (
+        <Fragment>
+            <h1 className="registro">Registrar Page</h1>
+        </Fragment>
+    )
+}
+
+export default RegisterPage;
