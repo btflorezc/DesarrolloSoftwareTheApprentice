319a660d47c83eb430ff8a03ff5f1994c6fcbb05
eot <eot1984@gmail.com>
2021-10-14 17:13:11 -0500

    react

---
+++ misiontic-shop-antonela/src/admon-ventas/VentasPage.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+function VentasPage() {
+
+    return (
+        <Fragment>
+            <h1>Ventas Page</h1>
+        </Fragment>
+    )
+}
+
+export default VentasPage;
