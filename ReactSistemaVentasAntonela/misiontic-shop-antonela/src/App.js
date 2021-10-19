9c2277c9895dc7468c989a61980104ea1140817d
amalzate821 <ana.alzate@udea.edu.co>
2021-10-18 23:19:58 -0500

    React Conectada con API

--- ReactSistemaVentasAntonela/misiontic-shop-antonela/src/App.js
+++ ReactSistemaVentasAntonela/misiontic-shop-antonela/src/App.js
@@ -3,6 +3,7 @@ import ProductosPage from "./productos/ProductosPage";
 import VentasPage from "./admon-ventas/VentasPage";
 import GestionUsuarios from "./gestion-usuarios/GestionUsuarios";
 import ActualizarProductosPage from "./productos/actualizar-productos/ActualizarProductosPage";
+import ListarPage from "./productos/listar-productos/ListarPage";
 import HomePage from "./home/HomePage";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import NavbarComponent from "./shared/components/navbar/NavbarComponent";
@@ -11,10 +12,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 
 function App() {
-  const {isAuthenticated} = useAuth0();
-  const validateUserRole = () => {
-    
-  }
+
   return (
     <Router>
       <NavbarComponent></NavbarComponent>
@@ -23,10 +21,13 @@ function App() {
           <HomePage/>
         </Route>
         <Route path="/productos" exact>
-        {isAuthenticated ? <ProductosPage />: <ForbidenComponent /> }
+        {grantAccess() ? <ProductosPage />: <ForbidenComponent /> }
         </Route>
         <Route path="/productos/actualizar-productos" exact>
         {isAuthenticated ?<ActualizarProductosPage />: <ForbidenComponent /> }
+        </Route>
+        <Route path="/productos/listar-productos" exact>
+          <ListarPage />
         </Route>
         <Route path="/admon-ventas" exact>
         {isAuthenticated ?<VentasPage />: <ForbidenComponent /> }
