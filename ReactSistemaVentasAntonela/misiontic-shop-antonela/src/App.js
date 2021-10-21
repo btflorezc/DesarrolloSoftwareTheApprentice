4234937cabb6ae3b8b341a265df6101af7b5011c
amalzate821 <ana.alzate@udea.edu.co>
2021-10-20 22:55:29 -0500

    Acceso según rol

--- ReactSistemaVentasAntonela/misiontic-shop-antonela/src/App.js
+++ ReactSistemaVentasAntonela/misiontic-shop-antonela/src/App.js
@@ -1,11 +1,11 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import ProductosPage from "./productos/ProductosPage";
 import VentasPage from "./admon-ventas/VentasPage";
 import GestionUsuarios from "./gestion-usuarios/GestionUsuarios";
 import ActualizarProductosPage from "./productos/actualizar-productos/ActualizarProductosPage";
 import ListarPage from "./productos/listar-productos/ListarPage";
 import HomePage from "./home/HomePage";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavbarComponent from "./shared/components/navbar/NavbarComponent";
 import ForbidenComponent from "./shared/components/forbiden/ForbidenComponent";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -17,29 +17,29 @@ function App() {
     <Router>
       <NavbarComponent></NavbarComponent>
       <Switch>
-      <Route path="/" exact>
-          <HomePage/>
+        <Route path="/" exact>
+          <HomePage />
         </Route>
         <Route path="/productos" exact>
-        {grantAccess() ? <ProductosPage />: <ForbidenComponent /> }
+          {isAuthenticated ? <ProductosPage /> : <ForbidenComponent />}
         </Route>
         <Route path="/productos/actualizar-productos" exact>
-        {isAuthenticated ?<ActualizarProductosPage />: <ForbidenComponent /> }
+          {isAuthenticated ? <ActualizarProductosPage /> : <ForbidenComponent />}
         </Route>
         <Route path="/productos/listar-productos" exact>
           <ListarPage />
         </Route>
         <Route path="/admon-ventas" exact>
-        {isAuthenticated ?<VentasPage />: <ForbidenComponent /> }
+          {isAuthenticated ? <VentasPage /> : <ForbidenComponent />}
         </Route>
         <Route path="/gestion-usuarios" exact>
-        {isAuthenticated ?<GestionUsuarios/>: <ForbidenComponent /> }
+          {isAuthenticated ? <GestionUsuarios /> : <ForbidenComponent />}
         </Route>
         <Route path="forbiden" exact>
           <ForbidenComponent />
         </Route>
       </Switch>
-    
+
     </Router>
   );
 }
